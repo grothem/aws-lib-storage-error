f6939c2ba35be241927f7c2ba3ebcfcaa9fc0591
Mark Grothe <mgrothe@iolap.com>
2021-07-12 15:22:36 -0500

    Repo aws lib-storage and webpack issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { Upload } from "@aws-sdk/lib-storage";
+import { S3Client, S3 } from "@aws-sdk/client-s3";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aws-lib-storage';
+
+  async test(Bucket: any, Key: any, Body: any) {
+    const target = { Bucket, Key, Body };
+    try {
+      const parallelUploads3 = new Upload({
+        client: new S3({}) || new S3Client({}),
+        params: target,
+      });
+
+      parallelUploads3.on("httpUploadProgress", (progress) => {
+        console.log(progress);
+      });
+
+      await parallelUploads3.done();
+    } catch (e) {
+      console.log(e);
+    }
+  }
 }
